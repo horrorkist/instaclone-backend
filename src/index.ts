9f832384c5e506918dc3433af6f2cd76d1c43263
horrorkist <horrorkist@gmail.com>
2023-12-02 08:05:24 +0900

    fsa

--- src/index.ts
+++ src/index.ts
@@ -67,24 +67,23 @@ const server = new ApolloServer({
 
 await server.start();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 app.use(
-  "/",
+  "/*",
   cors<cors.CorsRequest>({
-    origin: "https://web-instaclone-web-57lz2alpn2fku5.sel4.cloudtype.app/",
-    credentials: true,
+    origin: "*",
   }),
   express.json(),
   expressMiddleware(server, {
