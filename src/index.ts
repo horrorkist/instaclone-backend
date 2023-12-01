120d2f386a48076c3317cbb648eaa28511bd8f72
horrorkist <horrorkist@gmail.com>
2023-12-02 07:57:44 +0900

    cors

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,19 @@ const server = new ApolloServer({
 
 await server.start();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(
   "/",
   cors<cors.CorsRequest>({
